9467da632368fb05ac90dbc34b56086f1580f386
SquidR <98190190+SquidR@users.noreply.github.com>
2023-03-30 16:24:32 -0500

    Create Depth.lua

---
+++ Depth.lua
@@ -0,0 +1,90 @@
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/SquidR/Doors-Entites/main/fixspawner.lua"))()
+
+-- Create entity
+local entityTable = Spawner.createEntity({
+    CustomName = "Example", -- Custom name of your entity
+    Model = "PutEntityHere", -- Can be GitHub file or rbxassetid
+    Speed = 300, -- Percentage, 100 = default Rush speed
+    Ai = 1
+    DelayTime = 2, -- Time before starting cycles (seconds)
+    HeightOffset = 5,
+    CanKill = true,
+    KillRange = 100,
+    BackwardsMovement = false,
+    BreakLights = true,
+    FlickerLights = {
+        true, -- Enabled/Disabled
+        2, -- Time (seconds)
+    },
+    Cycles = {
+        Min = 4,
+        Max = 7,
+        WaitTime = 2,
+    },
+    CamShake = {
+        true, -- Enabled/Disabled
+        {3.5, 1, 0.1, 1}, -- Shake values (don't change if you don't know)
+        25, -- Shake start distance (from Entity to you)
+    },
+    Jumpscare = {
+        true, -- Enabled/Disabled
+        {
+            Image1 = "rbxassetid://11278229132", -- Image1 url
+            Image2 = "rbxassetid://11278229132", -- Image2 url
+            Shake = true,
+            Sound1 = {
+                10483790459, -- SoundId
+                { Volume = 0.5 }, -- Sound properties
+            },
+            Sound2 = {
+                10483790459, -- SoundId
+                { Volume = 1 }, -- Sound properties
+            },
+            Flashing = {
+                true, -- Enabled/Disabled
+                Color3.fromRGB(0,60,200), -- Color
+            },
+            Tease = {
+                false, -- Enabled/Disabled
+                Min = 1,
+                Max = 3,
+            },
+        },
+    },
+    CustomDialog = {"You have died to Depth...", "They are extremely fast, so hide quickly!"}, -- Custom death message
+})
+
+
+-----[[  Debug -=- Advanced  ]]-----
+entityTable.Debug.OnEntitySpawned = function()
+    print("Entity has spawned:", entityTable)
+end
+
+entityTable.Debug.OnEntityDespawned = function()
+    print("Entity has despawned:", entityTable)
+end
+
+entityTable.Debug.OnEntityStartMoving = function()
+    print("Entity has started moving:", entityTable)
+end
+
+entityTable.Debug.OnEntityFinishedRebound = function()
+    print("Entity has finished rebound:", entityTable)
+end
+
+entityTable.Debug.OnEntityEnteredRoom = function(room)
+    print("Entity:", entityTable, "has entered room:", room)
+end
+
+entityTable.Debug.OnLookAtEntity = function()
+    print("Player has looked at entity:", entityTable)
+end
+
+entityTable.Debug.OnDeath = function()
+    warn("Player has died.")
+end
+------------------------------------
+
+
+-- Run the created entity
+Spawner.runEntity(entityTable)
